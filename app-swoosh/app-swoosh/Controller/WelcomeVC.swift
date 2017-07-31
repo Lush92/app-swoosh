8cffd06b48e80d4c855184a2688ea8224954ef51
Lush <tdsconstantin@gmail.com>
2017-07-31 17:58:51 +0200

    link-segue

--- app-swoosh/app-swoosh/Controller/WelcomeVC.swift
+++ app-swoosh/app-swoosh/Controller/WelcomeVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  WelcomeVC.swift
 //  app-swoosh
 //
 //  Created by Constantin on 28/07/2017.
@@ -10,15 +10,15 @@ import UIKit
 
 class WelcomeVC: UIViewController {
 
-    @IBOutlet weak var swoosh: UIImageView!
-    @IBOutlet weak var bgImg: UIImageView!
     
     
     override func  viewDidLoad() {
          super.viewDidLoad()
     }
-    @IBAction func unwindFromSkillVC(unwindSegue: UIStoryboardSegue) {
     
+    
+    @IBAction func unwindFromSkillVC(unwindSegue: UIStoryboardSegue) {
+        
     }
 }
 
